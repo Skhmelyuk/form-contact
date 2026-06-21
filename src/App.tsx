7bc6b79cd045b9d7f6b6787ee66f6311a18b58f8
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-06-21 19:27:53 +0300

    add theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { Button } from "@/components/ui/button"
+import { useTheme } from "./components/theme-provider"
+import { Sun, Moon } from "lucide-react"
 
 export function App() {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
+    <div className="flex min-h-svh items-center justify-center p-6">
+      <div className="absolute top-5 right-5 flex items-center gap-4">
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
         </div>
+        <Button
+          variant="outline"
+          className="cursor-pointer"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? <Moon /> : <Sun />}
+        </Button>
       </div>
     </div>
   )
